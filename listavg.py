36a358a0ff0e47e09438fc5437bc45dc63939735
Tofunmi Odusile <tofunmi.odusile@kibo.school>
2024-01-10 23:12:54 +0100

    learning

--- listavg.py
+++ listavg.py
@@ -1,10 +1,18 @@
+from operator import add
+from functools import reduce
+
 class ListAverage:
     def __init__(self, lst):
         self.lst = lst.copy()
+        self.total = reduce(add, lst)
+    
+    def __str__(self):
+        return str(self.lst)
 
     def add(self, num):
         self.lst.append(num)
-
+        self.total += num
+        
     def compute_avg(self):
         total = 0
         for num in self.lst:
@@ -13,4 +21,47 @@ class ListAverage:
 
     def compute_avg_faster(self):
         # implement this method
-        pass
+        return self.total/len(self.lst)
+
+
+# # Create an instance of ListAverage
+# test_loop = ListAverage([1,2,3])
+# print(test_loop.compute_avg())
+
+# test_loop.add(6)
+# print(test_loop) 
+
+# test_reduce = ListAverage([1, 2, 3, 4])
+# print(test_reduce.compute_avg_faster())
+
+# test_reduce.add(5)
+# print(test_reduce)
+# print(test_reduce.compute_avg_faster())
+
+
+
+
+
+
+
+
+
+
+
+# class ListAverage:
+#     def __init__(self, lst):
+#         self.lst = lst.copy()
+
+#     def add(self, num):
+#         self.lst.append(num)
+
+# # Create an instance of ListAverage
+# my_list_average = ListAverage([1, 2, 3, 4, 5])
+
+# # Add a number to the list
+# my_list_average.add(6)
+
+# # Print the updated list
+# print(my_list_average.lst)
+
+
--- test_listavg.py
+++ test_listavg.py
@@ -7,3 +7,14 @@ class TestListAverage(unittest.TestCase):
         assert lavg.compute_avg() == 2.0
 
     # add more unit tests below
+    def test_faster(self):
+        faster_avg = ListAverage([1,2,3])
+        assert faster_avg.compute_avg_faster() == 2.0
+
+    def test_faster_add(self):
+        faster_add = ListAverage([1,2,3])
+        faster_add.add(6)
+        assert faster_add.compute_avg_faster() == 3.0
+
+
+    
